6f4b7f7aa1e1824b1c7342d3938a74f0dec5c514
lofiandcode <josephtrettevik@gmail.com>
2022-04-20 14:39:24 +0300

    Create express app and write a get request endpoint

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res, next) => {
+    console.log('Hello World');
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
